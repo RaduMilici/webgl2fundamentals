efc8d93b3d9514c94cf2f84afe62dd327aa908ac
RaduMilici <Radu.Milici.3pg@phishme.com>
2019-07-20 21:10:44 +0300

    nuke

--- fundamentals/src/const/trianglePoints.js
+++
@@ -1,7 +0,0 @@
-const trianglePoints = [
-  -1, 1,
-  1, 1,
-  0, -1
-];
-
-export default new Float32Array(trianglePoints);
--- fundamentals/src/resize.js
+++
@@ -1,7 +0,0 @@
-const resize = canvas => {
-  const { clientWidth, clientHeight } = canvas;
-  canvas.width = clientWidth;
-  canvas.height = clientHeight;
-};
-
-export default resize;
