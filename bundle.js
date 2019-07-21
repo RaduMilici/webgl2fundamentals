501432b8a3b6b5b3e5fedd967a0ed76d39adbf63
RaduMilici <Radu.Milici.3pg@phishme.com>
2019-07-21 20:21:36 +0300

    triangle

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   class Gl {
@@ -30,11 +30,9 @@
     }
   }
 
-  var fragmentShaderSource =
-    '#version 300 es\nprecision mediump float;out vec4 color;void main(){color=vec4(0.,0.,1.,1.);}';
+  var fragmentShaderSource = "#version 300 es\nprecision mediump float;out vec4 color;void main(){color=vec4(0.,0.,1.,1.);}";
 
-  var vertexShaderSource =
-    '#version 300 es\nin vec2 a_position;uniform float uPointSize;void main(){gl_PointSize=uPointSize;gl_Position=vec4(a_position,0.,1.);}';
+  var vertexShaderSource = "#version 300 es\nin vec2 a_position;uniform float uPointSize;void main(){gl_PointSize=uPointSize;gl_Position=vec4(a_position,0.,1.);}";
 
   const deleteShader = ({ context, program, shader }) => {
     context.detachShader(program, shader);
@@ -122,27 +120,56 @@
     validate: true,
   });
 
-  context.useProgram(program);
-
   // POINTS
   const aPositionLoc = context.getAttribLocation(program, 'a_position');
   const uPointSizeLoc = context.getUniformLocation(program, 'uPointSize');
-  const vertsArray = new Float32Array([0, 0, 0.5, 0.5]);
+  const vertsArray = new Float32Array([0.8, -0.8]);
   const vertsBuffer = context.createBuffer();
 
   context.bindBuffer(context.ARRAY_BUFFER, vertsBuffer);
   context.bufferData(context.ARRAY_BUFFER, vertsArray, context.STATIC_DRAW);
-  context.enableVertexAttribArray(aPositionLoc);
 
   const size = 2; // 2 components per iteration
   const type = context.FLOAT; // the data is 32bit floats
-  const normalize = false; // don't normalize the data
-  const stride = 0; // 0 means iterate size * sizeof(type) to get next index
+  const normalize = context.FALSE; // don't normalize the data
+  const stride = size * Float32Array.BYTES_PER_ELEMENT; // 0 means iterate size * sizeof(type) to get next index
   const offset = 0; // start at the beginning of the buffer
+  // https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/vertexAttribPointer
   context.vertexAttribPointer(aPositionLoc, size, type, normalize, stride, offset);
+  context.enableVertexAttribArray(aPositionLoc);
   context.bindBuffer(context.ARRAY_BUFFER, null);
+  context.useProgram(program);
   context.uniform1f(uPointSizeLoc, 10);
-  context.drawArrays(context.POINTS, 0, 2);
+  context.drawArrays(context.POINTS, 0, vertsArray.length / size);
+
+  function random(min = 0, max = 1) {
+    return Math.random() * (max - min) + min;
+  }
+
+  gl.setClearColor({ r: 0, g: 0, b: 0, a: 1 });
+
+  const animate = () => {
+    gl.clear();
+
+    const posArray = [];
+    const count = 100;
+
+    for (let i = 0; i < count; i++) {
+      const x = random(-1, 1);
+      const y = random(-1, 1);
+      posArray.push(x, y);
+    }
+
+    const floatArray = new Float32Array(posArray);
+    context.bindBuffer(context.ARRAY_BUFFER, vertsBuffer);
+    context.bufferData(context.ARRAY_BUFFER, floatArray, context.STATIC_DRAW);
+    context.drawArrays(context.POINTS, 0, count);
+    context.bindBuffer(context.ARRAY_BUFFER, null);
+  };
+
+  animate();
+
+  // setInterval(animate, 100);
 
   // TRIANGLE
   const trangleVertsBuffer = context.createBuffer();
@@ -150,30 +177,9 @@
   context.bindBuffer(context.ARRAY_BUFFER, trangleVertsBuffer);
   context.bufferData(context.ARRAY_BUFFER, trangleVertsArray, context.STATIC_DRAW);
   context.vertexAttribPointer(aPositionLoc, size, type, normalize, stride, offset);
-  context.drawArrays(context.TRIANGLES, 0, 3);
+  context.drawArrays(context.TRIANGLES, 0, trangleVertsArray.length / 2);
+
+  //context.useProgram(null);
   context.useProgram(null);
 
-  /*
-  function random(min = 0, max = 1) {
-    return Math.random() * (max - min) + min;
-  }
-
-  const animate = () => {
-    gl.setClearColor({ r: random(), g: random(), b: random(), a: 1 });
-    gl.clear();
-    const posArray = [];
-    const count = 500;
-    for (let i = 0; i < count; i++) {
-      const x = random(-1, 1);
-      const y = random(-1, 1);
-      posArray.push(x, y);
-    }
-    const floatArray = new Float32Array(posArray);
-    context.bindBuffer(context.ARRAY_BUFFER, vertsBuffer);
-    context.bufferData(context.ARRAY_BUFFER, floatArray, context.STATIC_DRAW);
-    context.drawArrays(context.POINTS, 0, count);
-    context.bindBuffer(context.ARRAY_BUFFER, null);
-  };
-  animate();
-  context.useProgram(null);*/
-})();
+}());
